3fb81af672e90599db0dadea4b01d83830803634
Matheus <matheus.oliveira.luna.garcia@gmail.com>
2025-12-17 02:30:20 -0300

    Mais mudanças no app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.seniorcare',
   appName: 'bem-estar-social',
   webDir: 'dist',
-  server: {
-    url: 'https://3a1003fd-c05f-4cb7-8870-1dc3dc42a0d5.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
   plugins: {
     SplashScreen: {
       launchShowDuration: 0
